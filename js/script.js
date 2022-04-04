86148c714a35c7c4d3adcc975dc64d2830026676
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-04 17:29:26 +0000

    4 changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 // This file contains the JS functions for index.html
 
 'use strict'
+
 /**
  * This function calculates 
  */
-function calculate()
+function calculate() { 
+  // input
+  const radius = parseFloat(document.getElementById('radius-of-sphere').value)
+
+  // process
+  const volume = 4/3 * Math.PI * Math.pow(radius, 3)
+  
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
